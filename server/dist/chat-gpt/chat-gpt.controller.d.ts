d631492febf8585924a60545306e1c844b780d60
Mahammad Zeynalov <magamed9r2.2011@gmail.com>
2024-08-04 21:38:16 +0400

    remove dist folder from git history

--- server/dist/chat-gpt/chat-gpt.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { ChatGptService } from './chat-gpt.service';
-export declare class ChatGptController {
-    private readonly chatGptService;
-    constructor(chatGptService: ChatGptService);
-    askChatGpt(prompt: string): Promise<any>;
-}
--- server/dist/chat-gpt/chat-gpt.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-export declare class ChatGptService {
-    private configService;
-    private readonly apiKey;
-    private readonly apiUrl;
-    constructor(configService: ConfigService);
-    getChatGptResponse(prompt: string): Promise<any>;
-}
